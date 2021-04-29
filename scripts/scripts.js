535f29edea1a9e33378b744cfbbc3135ef006e29
S67 <voidhollow321@gmail.com>
2021-04-29 14:57:31 +0900

    0.1.12a
    loading

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,14 +1,19 @@
 ///<reference path="../library/JQuery/typings/globals/jquery/index.d.ts" />
 window.onload = function() {
-  loading();
   audio();
+  loading();
 }
 
 function loading(){
   const spinner = document.getElementById('loading');
-  spinner.classList.add('loaded');
+  const logo = document.getElementById('loaded-logo');
+  window.setTimeout(function(){
+    spinner.classList.add('loaded');
+  },3000);
+  window.setTimeout(function(){
+    logo.classList.add('loaded');
+  },5000);
 }
-
 function audio(){
   const audio = document.getElementById('sound-file');
   audio.volume= 0.5;
