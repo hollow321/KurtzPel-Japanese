9c9b4e170c61ac46f61997f7f269832038cf1ffa
S67 <voidhollow321@gmail.com>
2021-04-29 09:03:12 +0900

    0.1.9a
    audio volume

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,5 +1,15 @@
 ///<reference path="../library/JQuery/typings/globals/jquery/index.d.ts" />
 window.onload = function() {
+  load();
+  audio();
+}
+
+function load(){
   const spinner = document.getElementById('loading');
   spinner.classList.add('loaded');
+}
+
+function audio(){
+  const audio = document.getElementById('sound-file');
+  audio.volume= 0.5;
 }
